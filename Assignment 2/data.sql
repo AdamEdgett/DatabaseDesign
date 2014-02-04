aa7db20b243138d78c72d1a2513d90d508c60a48
Adam Edgett <AdamEdgett@gmail.com>
2014-02-03 22:10:06 -0500

    Updated and finished schema and test data

--- Assignment 2/data.sql
+++ Assignment 2/data.sql
@@ -1,10 +1,104 @@
 -- Enforce foreign keys for sqlite
 PRAGMA foreign_keys = ON;
+-- Set output style for sqlite
+
+-- Populate enumerations
+INSERT INTO RoleType(name) VALUES
+    ("Business Analyst"),
+    ("Architect"),
+    ("Database Admin"),
+    ("Back End Developer"),
+    ("Front End Developer");
+
+INSERT INTO Browser(name) VALUES
+    ("Chrome"), ("Firefox"), ("Safari"), ("IE");
+
+INSERT INTO MobileOS(name) VALUES
+    ("Android"), ("iOS"), ("Blackberry");
+
+INSERT INTO DesktopOS(name) VALUES
+    ("Windows"), ("OSX"), ("Linux");
+
 INSERT INTO Developer(name) VALUES("Adam");
-INSERT INTO Application(name, created, price) VALUES("Google", "2014-1-31", 12);
-INSERT INTO WebApplication(app_id, url, targetbrowser) VALUES(1, "google.com", "chrome");
-INSERT INTO Role(type, devel_id, app_id) VALUES("Admin", 1, 1);
--- Should get an error due to FK constraint
-INSERT INTO Role(type, devel_id, app_id) VALUES("Developer", 2, 3);
-INSERT INTO Application(name, created, price) VALUES("Facebook", "2014-1-31", 20);
+INSERT INTO Developer(name) VALUES("John");
+INSERT INTO Developer(name) VALUES("Bob");
+INSERT INTO DEVELOPER(name) VALUES("Tim");
+
+-- Example Applications
+INSERT INTO Application(name, created, price) VALUES("Google", "2014-01-31", 12);
+INSERT INTO WebApplication(app_id, url, targetbrowser) VALUES(1, "google.com", "Chrome");
+INSERT INTO Role(type, devel_id, app_id) VALUES("Database Admin", 1, 1);
+INSERT INTO Application(name, created, price) VALUES("Facebook", "2014-01-31", 20);
 INSERT INTO MobileApplication(app_id, targetos) VALUES(2, "iOS");
+INSERT INTO Role(type, devel_id, app_id) VALUES("Back End Developer", 1, 2);
+INSERT INTO Application(name, created, price) VALUES("Sublime Text", "2012-05-13", 70);
+INSERT INTO DesktopApplication(app_id, targetos) VALUES(3, "Windows");
+INSERT INTO Role(type, devel_id, app_id) VALUES("Front End Developer", 2, 3);
+INSERT INTO Application(name, created) VALUES ("Dropbox", "2013-07-17");
+INSERT INTO WebApplication(app_id, url, targetbrowser) VALUES(4, "dropbox.com", "Firefox");
+INSERT INTO Role(type, devel_id, app_id) VALUES("Architect", 3, 4);
+INSERT INTO Application(name, created, price) VALUES("Angry Birds", "2011-04-11", 1);
+INSERT INTO MobileApplication(app_id, targetos) VALUES(5, "Blackberry");
+INSERT INTO Role(type, devel_id, app_id) VALUES("Business Analyst", 4, 5);
+
+-- Continue extension of first example application
+INSERT INTO Controller(app_id) VALUES(1);
+INSERT INTO Model(app_id, name) VALUES(1, "Searches");
+INSERT INTO View(app_id, name) VALUES(1, "Search Results");
+INSERT INTO Script(controller_id, model_id, name, path) VALUES(1, 1, "search", "/www/js/search.js");
+INSERT INTO EventHandler(view_id, script_id, name) VALUES(1, 1, "Run Search");
+INSERT INTO NameValuePair(model_id, name) VALUES(1, "Query");
+INSERT INTO StringNVP(nvp_id, value) VALUES(1, "northeastern");
+INSERT INTO Model(app_id, name) VALUES(1, "Results");
+INSERT INTO NameValuePair(model_id, name) VALUES(2, "Result Page 1");
+INSERT INTO NameValuePair(model_id, name) VALUES(2, "Search Date");
+INSERT INTO DateNVP(nvp_id, value) VALUES(3, "2014-02-1");
+INSERT INTO NameValuePair(model_id, name) VALUES(2, "northeastern.edu");
+INSERT INTO NumberNVP(nvp_id, value) VALUES(4, 1);
+INSERT INTO NameValueNVP(parent_nvp_id, value_nvp_id) VALUES(3, 4);
+
+-- Print all data
+SELECT * FROM RoleType;
+SELECT * FROM Browser;
+SELECT * FROM MobileOS;
+SELECT * FROM DesktopOS;
+SELECT * FROM Developer;
+SELECT * FROM Role;
+SELECT * FROM Application;
+SELECT * FROM MobileApplication;
+SELECT * FROM WebApplication;
+SELECT * FROM DesktopApplication;
+SELECT * FROM Controller;
+SELECT * FROM Model;
+SELECT * FROM View;
+SELECT * FROM Script;
+SELECT * FROM EventHandler;
+SELECT * FROM NameValuePair;
+SELECT * FROM StringNVP;
+SELECT * FROM NumberNVP;
+SELECT * FROM DateNVP;
+SELECT * FROM NameValueNVP;
+
+-- Print joined tables
+SELECT name, created, price, url, targetbrowser
+    FROM Application NATURAL JOIN WebApplication;
+SELECT Developer.name, Role.type, Application.name FROM Role
+    INNER JOIN Developer using (devel_id)
+    INNER JOIN Application using (app_id);
+
+-- Error testing
+-- Exepected error due to FK constraints
+INSERT INTO Role(type, devel_id, app_id) VALUES("Database Admin", 8, 7);
+-- Returns "Error: near line 91: FOREIGN KEY constraint failed"
+
+-- Expected error due to NOT NULL constraint on name
+INSERT INTO Application(created, price) VALUES("2014-1-31", 1);
+-- Returns "Error: near line 95: NOT NULL constraint failed: Application.name"
+
+-- Expected error due to PK constraint
+INSERT INTO DesktopApplication(deskapp_id, app_id, targetos) VALUES(1, 1, "Linux");
+-- Returns "Error: near line 99: UNIQUE constraint failed: DesktopApplication.deskapp_id"
+
+-- Expected error due to FK constraint
+INSERT INTO Script(controller_id, model_id, name, path) VALUES(11, 7, "test", "test.py");
+-- Returns "Error: near line 103: FOREIGN KEY constraint failed"
--- Assignment 2/schema.sql
+++ Assignment 2/schema.sql
@@ -1,138 +1,218 @@
-PRAGMA foreign_keys = ON;
+-- Drop tables
+DROP TABLE IF EXISTS Developer;
+DROP TABLE IF EXISTS Role;
+DROP TABLE IF EXISTS RoleType;
+DROP TABLE IF EXISTS Application;
+DROP TABLE IF EXISTS WebApplication;
+DROP TABLE IF EXISTS Browser;
+DROP TABLE IF EXISTS MobileApplication;
+DROP TABLE IF EXISTS MobileOS;
+DROP TABLE IF EXISTS DesktopApplication;
+DROP TABLE IF EXISTS DesktopOS;
+DROP TABLE IF EXISTS ApplicationAll;
+DROP TABLE IF EXISTS Controller;
+DROP TABLE IF EXISTS Script;
+DROP TABLE IF EXISTS View;
+DROP TABLE IF EXISTS EventHandler;
+DROP TABLE IF EXISTS Model;
+DROP TABLE IF EXISTS NameValuePair;
+DROP TABLE IF EXISTS StringNVP;
+DROP TABLE IF EXISTS NumberNVP;
+DROP TABLE IF EXISTS DateNVP;
+DROP TABLE IF EXISTS NameValueNVP;
+DROP TABLE IF EXISTS NameValuePairAll;
+
 -- Create tables
 CREATE TABLE Developer(
-    id INTEGER NOT NULL,
-    name VARCHAR(55) NOT NULL,
-    PRIMARY KEY(id)
+    devel_id INTEGER NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    PRIMARY KEY(devel_id)
 );
 CREATE TABLE Role(
-    id INTEGER NOT NULL,
-    type VARCHAR(55) NOT NULL,
+    role_id INTEGER NOT NULL,
+    type VARCHAR(50) NOT NULL,
     devel_id INTEGER NOT NULL,
     app_id INTEGER NOT NULL,
-    PRIMARY KEY(id),
-    FOREIGN KEY(devel_id) REFERENCES Developer(id)
+    PRIMARY KEY(role_id),
+    FOREIGN KEY(type) REFERENCES RoleType(name),
+    FOREIGN KEY(devel_id) REFERENCES Developer(devel_id)
     ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY(app_id) REFERENCES Application(id)
+    FOREIGN KEY(app_id) REFERENCES Application(app_id)
     ON DELETE CASCADE ON UPDATE CASCADE,
     UNIQUE(devel_id, app_id)
 );
+CREATE TABLE RoleType(
+    name VARCHAR(50) NOT NULL,
+    PRIMARY KEY(name)
+);
 
 CREATE TABLE Application(
-    id INTEGER NOT NULL,
-    name VARCHAR(55) NOT NULL,
+    app_id INTEGER NOT NULL,
+    name VARCHAR(50) NOT NULL,
     created DATE,
-    price DOUBLE,
-    PRIMARY KEY(id)
+    price DOUBLE DEFAULT 0,
+    PRIMARY KEY(app_id)
 );
 CREATE TABLE WebApplication(
-    id INTEGER NOT NULL,
+    webapp_id INTEGER NOT NULL,
     app_id INTEGER NOT NULL,
     url VARCHAR(200) NOT NULL,
-    targetbrowser VARCHAR(55),
-    PRIMARY KEY(id),
-    FOREIGN KEY(app_id) REFERENCES Application(id)
-    ON DELETE CASCADE ON UPDATE CASCADE
+    targetbrowser VARCHAR(50),
+    PRIMARY KEY(webapp_id),
+    FOREIGN KEY(app_id) REFERENCES Application(app_id)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY(targetbrowser) REFERENCES Browser(name)
+);
+CREATE TABLE Browser(
+    name VARCHAR(50) NOT NULL,
+    PRIMARY KEY(name)
 );
 CREATE TABLE MobileApplication(
-    id INTEGER NOT NULL,
+    mobileapp_id INTEGER NOT NULL,
     app_id INTEGER NOT NULL,
-    targetos VARCHAR(55),
+    targetos VARCHAR(50),
     installs INTEGER DEFAULT 0,
-    PRIMARY KEY(id),
-    FOREIGN KEY(app_id) REFERENCES Application(id)
-    ON DELETE CASCADE ON UPDATE CASCADE
+    PRIMARY KEY(mobileapp_id),
+    FOREIGN KEY(app_id) REFERENCES Application(app_id)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY(targetos) REFERENCES MobileOS(name)
+);
+CREATE TABLE MobileOS(
+    name VARCHAR(50) NOT NULL,
+    PRIMARY KEY(name)
 );
 CREATE TABLE DesktopApplication(
-    id INTEGER NOT NULL,
+    deskapp_id INTEGER NOT NULL,
     app_id INTEGER NOT NULL,
-    targetos VARCHAR(55),
-    PRIMARY KEY(id),
-    FOREIGN KEY(app_id) REFERENCES Application(id)
-    ON DELETE CASCADE ON UPDATE CASCADE
+    targetos VARCHAR(50),
+    PRIMARY KEY(deskapp_id),
+    FOREIGN KEY(app_id) REFERENCES Application(app_id)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY(targetos) REFERENCES DesktopOS(name)
+);
+CREATE TABLE DesktopOS(
+    name VARCHAR(50) NOT NULL,
+    PRIMARY KEY(name)
+);
+-- Single table of applications (unused)
+CREATE TABLE ApplicationAll(
+    app_id INTEGER NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    created DATE,
+    price DOUBLE DEFAULT 0,
+    installs INTEGER,
+    targetbrowser VARCHAR(50),
+    targetmobileos VARCHAR(50),
+    targetdesktopos VARCHAR(50),
+    PRIMARY KEY(app_id),
+    FOREIGN KEY(targetbrowser) REFERENCES Browser(name),
+    FOREIGN KEY(targetmobileos) REFERENCES MobileOS(name),
+    FOREIGN KEY(targetdesktopos) REFERENCES DesktopOS(name)
 );
 
 CREATE TABLE Controller(
-    id INTEGER NOT NULL,
+    controller_id INTEGER NOT NULL,
     app_id INTEGER NOT NULL,
-    PRIMARY KEY(id),
-    FOREIGN KEY(app_id) REFERENCES Application(id)
+    PRIMARY KEY(controller_id),
+    FOREIGN KEY(app_id) REFERENCES Application(app_id)
     ON DELETE CASCADE ON UPDATE CASCADE
 );
 CREATE TABLE Script(
-    id INTEGER NOT NULL,
+    script_id INTEGER NOT NULL,
     controller_id INTEGER NOT NULL,
     model_id INTEGER NOT NULL,
-    PRIMARY KEY(id),
-    FOREIGN KEY(controller_id) REFERENCES Controller(id)
+    name VARCHAR(50) NOT NULL,
+    path VARCHAR(255) NOT NULL,
+    PRIMARY KEY(script_id),
+    FOREIGN KEY(controller_id) REFERENCES Controller(controller_id)
     ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY(model_id) REFERENCES Model(id)
+    FOREIGN KEY(model_id) REFERENCES Model(model_id)
 );
 
 CREATE TABLE View(
-    id INTEGER NOT NULL,
+    view_id INTEGER NOT NULL,
     app_id INTEGER NOT NULL,
-    superview_id INTEGER NOT NULL,
-    PRIMARY KEY(id),
-    FOREIGN KEY(app_id) REFERENCES Application(id)
+    name VARCHAR(50) NOT NULL,
+    superview_id INTEGER,
+    PRIMARY KEY(view_id),
+    FOREIGN KEY(app_id) REFERENCES Application(app_id)
     ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY(superview_id) REFERENCES View(id)
+    FOREIGN KEY(superview_id) REFERENCES View(view_id)
     ON DELETE CASCADE ON UPDATE CASCADE
 );
 CREATE TABLE EventHandler(
-    id INTEGER NOT NULL,
+    eventhandler_id INTEGER NOT NULL,
     view_id INTEGER NOT NULL,
     script_id INTEGER NOT NULL,
-    PRIMARY KEY(id),
-    FOREIGN KEY(view_id) REFERENCES View(id)
+    name VARCHAR(50) NOT NULL,
+    PRIMARY KEY(eventhandler_id),
+    FOREIGN KEY(view_id) REFERENCES View(view_id)
     ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY(script_id) REFERENCES Script(id)
+    FOREIGN KEY(script_id) REFERENCES Script(script_id)
 );
 
 CREATE TABLE Model(
-    id INTEGER NOT NULL,
+    model_id INTEGER NOT NULL,
     app_id INTEGER NOT NULL,
-    PRIMARY KEY(id),
-    FOREIGN KEY(app_id) REFERENCES Application(id)
+    name VARCHAR(50) NOT NULL,
+    PRIMARY KEY(model_id),
+    FOREIGN KEY(app_id) REFERENCES Application(app_id)
     ON DELETE CASCADE ON UPDATE CASCADE
 );
 CREATE TABLE NameValuePair(
-    id INTEGER NOT NULL,
+    nvp_id INTEGER NOT NULL,
     model_id INTEGER NOT NULL,
     name VARCHAR(100) NOT NULL,
-    PRIMARY KEY(id),
-    FOREIGN KEY(model_id) REFERENCES Model(id)
+    PRIMARY KEY(nvp_id),
+    FOREIGN KEY(model_id) REFERENCES Model(model_id)
     ON DELETE CASCADE ON UPDATE CASCADE
 );
 CREATE TABLE StringNVP(
-    id INTEGER NOT NULL,
+    stringnvp_id INTEGER NOT NULL,
     nvp_id INTEGER NOT NULL,
     value VARCHAR(255) NOT NULL,
-    PRIMARY KEY(id),
-    FOREIGN KEY(nvp_id) REFERENCES NameValuePair(id)
+    PRIMARY KEY(stringnvp_id),
+    FOREIGN KEY(nvp_id) REFERENCES NameValuePair(nvp_id)
     ON DELETE CASCADE ON UPDATE CASCADE
 );
 CREATE TABLE NumberNVP(
-    id INTEGER NOT NULL,
+    numnvp_id INTEGER NOT NULL,
     nvp_id INTEGER NOT NULL,
     value INTEGER NOT NULL,
-    PRIMARY KEY(id),
-    FOREIGN KEY(nvp_id) REFERENCES NameValuePair(id)
+    PRIMARY KEY(numnvp_id),
+    FOREIGN KEY(nvp_id) REFERENCES NameValuePair(nvp_id)
     ON DELETE CASCADE ON UPDATE CASCADE
 );
 CREATE TABLE DateNVP(
-    id INTEGER NOT NULL,
+    datenvp_id INTEGER NOT NULL,
     nvp_id INTEGER NOT NULL,
     value DATE NOT NULL,
-    PRIMARY KEY(id),
-    FOREIGN KEY(nvp_id) REFERENCES NameValuePair(id)
+    PRIMARY KEY(datenvp_id),
+    FOREIGN KEY(nvp_id) REFERENCES NameValuePair(nvp_id)
     ON DELETE CASCADE ON UPDATE CASCADE
 );
 CREATE TABLE NameValueNVP(
-    id INTEGER NOT NULL,
-    nvp_id INTEGER NOT NULL,
-    value INTEGER NOT NULL,
-    PRIMARY KEY(id),
-    FOREIGN KEY(nvp_id) REFERENCES NameValuePair(id)
+    nvpnvp_id INTEGER NOT NULL,
+    parent_nvp_id INTEGER NOT NULL,
+    value_nvp_id INTEGER NOT NULL,
+    PRIMARY KEY(nvpnvp_id),
+    FOREIGN KEY(parent_nvp_id) REFERENCES NameValuePair(nvp_id)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY(value_nvp_id) REFERENCES NameValuePair(nvp_id)
     ON DELETE CASCADE ON UPDATE CASCADE
 );
+-- Single table of name value pairs (unused)
+CREATE TABLE NameValuePairAll(
+    nvp_id INTEGER NOT NULL,
+    model_id INTEGER NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    stringvalue VARCHAR(255),
+    numbervalue INTEGER,
+    datevalue DATE,
+    nvpvalue INTEGER,
+    PRIMARY KEY(nvp_id),
+    FOREIGN KEY(model_id) REFERENCES Model(model_id)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY(nvpvalue) REFERENCES NameValuePairAll(nvp_id)
+);
+

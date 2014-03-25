9769872a8b3188128ea723331ec25260a1484d69
Adam Edgett <AdamEdgett@gmail.com>
2014-03-25 00:08:01 -0400

    Added entity managers

---
+++ Assignment 4/WEB-INF/src/dbd/CastManager.java
@@ -0,0 +1,212 @@
+package dbd;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+
+public class CastManager {
+    DataSource ds;
+    Connection connection = null;
+    PreparedStatement statement = null;
+    ResultSet results = null;
+
+    String createCast = "INSERT INTO Cast (id, characterName, movieId, actorId) VALUES (?, ?, ?, ?)";
+    String readAllCast = "SELECT * FROM Cast";
+    String readAllCastForActor = "SELECT * FROM Cast WHERE actorId=?";
+    String readAllCastForMovie = "SELECT * FROM Cast WHERE movieId=?";
+    String readCastForId = "SELECT * FROM Cast WHERE Id=?";
+    String updateCast = "UPDATE Cast SET id=?, characterName=?, actorId=?, movieId=? WHERE id=?";
+    String deleteCast = "DELETE FROM Cast WHERE id=?";
+
+    public CastManager() {
+        try {
+            Context jndi = new InitialContext();
+            ds = (DataSource) jndi.lookup("java:comp/env/jdbc/dbd");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void createCast(Cast newCast) {
+        try {
+            connection = ds.getConnection();
+            statement = connection.prepareStatement(createCast);
+            statement.setObject(1, newCast);
+            statement.setString(2, newCast.getCharacterName());
+            statement.setInt(3, newCast.getMovieId());
+            statement.setInt(4, newCast.getActorId());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public List readAllCast() {
+        List<Cast> casts = new ArrayList<Cast>();
+
+        try {
+            connection = ds.getConnection();
+            statement = connection.prepareStatement(readAllCast);
+            results = statement.executeQuery();
+            while (results.next()) {
+                Cast cast = new Cast(
+                        results.getInt("id"),
+                        results.getString("characterName"),
+                        results.getInt("movieId"),
+                        results.getInt("actorId")
+                );
+                casts.add(cast);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return casts;
+    }
+
+    public List readAllCastForActor(Integer actorId) {
+        List<Cast> casts = new ArrayList<Cast>();
+
+        try {
+            connection = ds.getConnection();
+            statement = connection.prepareStatement(readAllCastForActor);
+            statement.setInt(1, actorId);
+            results = statement.executeQuery();
+            while (results.next()) {
+                Cast cast = new Cast(
+                        results.getInt("id"),
+                        results.getString("characterName"),
+                        results.getInt("movieId"),
+                        results.getInt("actorId")
+                );
+                casts.add(cast);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return casts;
+    }
+
+    public List readAllCastForMovie(Integer movieId) {
+        List<Cast> casts = new ArrayList<Cast>();
+
+        try {
+            connection = ds.getConnection();
+            statement = connection.prepareStatement(readAllCastForMovie);
+            statement.setInt(1, movieId);
+            results = statement.executeQuery();
+            while (results.next()) {
+                Cast cast = new Cast(
+                        results.getInt("id"),
+                        results.getString("characterName"),
+                        results.getInt("movieId"),
+                        results.getInt("actorId")
+                );
+                casts.add(cast);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return casts;
+    }
+
+    public Cast readCastForId(Integer castId) {
+        try {
+            connection = ds.getConnection();
+            statement = connection.prepareStatement(readCastForId);
+            statement.setInt(1, castId);
+            results = statement.executeQuery();
+            if (results.next()) {
+                return new Cast(
+                        results.getInt("id"),
+                        results.getString("characterName"),
+                        results.getInt("movieId"),
+                        results.getInt("actorId")
+                );
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return null;
+    }
+
+    public void updateCast(Integer castId, Cast newCast) {
+        try {
+            connection = ds.getConnection();
+            statement = connection.prepareStatement(updateCast);
+            statement.setInt(1, newCast.getId());
+            statement.setString(2, newCast.getCharacterName());
+            statement.setInt(3, newCast.getActorId());
+            statement.setInt(4, newCast.getMovieId());
+            statement.setInt(5, castId);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void deleteCast(Integer castId) {
+        try {
+            connection = ds.getConnection();
+            statement = connection.prepareStatement(deleteCast);
+            statement.setInt(1, castId);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ Assignment 4/WEB-INF/src/dbd/MovieManager.java
@@ -0,0 +1,151 @@
+package dbd;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Date;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class MovieManager {
+    DataSource ds;
+    Connection connection = null;
+    PreparedStatement statement = null;
+    ResultSet results = null;
+
+    String createMovie = "INSERT INTO Movie" +
+            "(id, title, posterImage, releaseDate) VALUES (?, ?, ?, ?)";
+    String readAllMovies = "SELECT * FROM Movie";
+    String readMovie = "SELECT * FROM Movie WHERE id = ?";
+    String updateMovie = "UPDATE Movie SET id=?, title=?, posterImage=?, releaseDate=? WHERE id=?";
+    String deleteMovie = "DELETE FROM Movie WHERE id=?";
+
+    public MovieManager() {
+        try {
+            Context jndi = new InitialContext();
+            ds = (DataSource) jndi.lookup("java:comp/env/jdbc/dbd");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void createMovie(Movie newMovie) {
+        try {
+            connection = ds.getConnection();
+            statement = connection.prepareStatement(createMovie);
+            statement.setObject(1, newMovie);
+            statement.setString(2, newMovie.getTitle());
+            statement.setString(3, newMovie.getPosterImage());
+            statement.setDate(4, new Date(newMovie.getReleaseDate().getTime()));
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public List<Movie> readAllMovies() {
+        List<Movie> movies = new ArrayList<Movie>();
+
+        try {
+            connection = ds.getConnection();
+            statement = connection.prepareStatement(readAllMovies);
+            results = statement.executeQuery();
+            while (results.next()) {
+                Movie movie = new Movie(
+                        results.getInt("id"),
+                        results.getString("title"),
+                        results.getString("posterImage"),
+                        results.getDate("releaseDate")
+                );
+                movies.add(movie);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return movies;
+    }
+
+    public Movie readMovie(Integer movieId) {
+        try {
+            connection = ds.getConnection();
+            statement = connection.prepareStatement(readMovie);
+            statement.setInt(1, movieId);
+            results = statement.executeQuery();
+            if (results.next()) {
+                return new Movie(
+                        results.getInt("id"),
+                        results.getString("title"),
+                        results.getString("posterImage"),
+                        results.getDate("releaseDate")
+                );
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return null;
+    }
+    public void updateMovie(Integer movieId, Movie movie) {
+        try {
+            connection = ds.getConnection();
+            statement = connection.prepareStatement(updateMovie);
+            statement.setInt(1, movie.getId());
+            statement.setString(2, movie.getTitle());
+            statement.setString(3, movie.getPosterImage());
+            statement.setDate(4, new Date(movie.getReleaseDate().getTime()));
+            statement.setInt(5, movieId);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void deleteMovie(Integer movieId) {
+        try {
+            connection = ds.getConnection();
+            statement = connection.prepareStatement(deleteMovie);
+            statement.setInt(1, movieId);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
